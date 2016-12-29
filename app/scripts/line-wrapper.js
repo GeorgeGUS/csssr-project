b8b78a3e45d98b665bdcb8687de56ff29b70717f
GeorgeGUS <georgegus1@gmail.com>
2016-12-29 18:28:28 +0300

    Fixed lint issues

--- app/scripts/line-wrapper.js
+++ app/scripts/line-wrapper.js
@@ -4,7 +4,7 @@ $(() => {
 	const text = $('.underlined-text');
 	const textWords = text[0].innerText.split(' ');
 	for (let i = 0; i < textWords.length; i++) {
-
+		// write words
 	}
 	if (text[0].scrollWidth > text.innerWidth()) {
 		// Text has over-flowed
